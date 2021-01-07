bcae3e5b80a64212a81910c5c905b61254ce3c9a
Deepak Kumar Jain <deepakkumarjain21@gmail.com>
2021-01-08 00:34:40 +0530

    use sessionStorage instead of chrom storage api for toggling hide/show of extension app in DOM

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -5,39 +5,19 @@ console.log('Content script works!');
 console.log('Must reload extension for modifications to take effect.');
 
 function receivefunc(mssg, sender, sendResponse) {
-  // console.log("mssg", mssg);
-  // console.log("sender", sender);
-  // console.log("sendResponse", sendResponse);
+  let isAppOpen = JSON.parse(sessionStorage.getItem('app_open_status')) || false;
 
-  chrome.storage.sync.get(['app_open_status'], function (data) {
-    console.log('app_open_status', data);
-    let isAppOpen = data.app_open_status;
-    if (!isAppOpen) {
-      chrome.storage.sync.set({ app_open_status: true }, function () {
-        console.log('app_open_status is set to true');
-        mountApp();
-      });
-    } else {
-      chrome.storage.sync.set({ app_open_status: false }, function () {
-        console.log('app_open_status is set to false');
-        unmountApp();
-      });
-    }
-  });
-  // chrome.storage.sync.set({ color: '#3aa757'}, function() {
-  //   console.log("The color is green.");
-  // });
+
   if (mssg.type === 'toggleApp') {
-    // mountApp();
-    /*  
-       your code of content script goes here
-    */
-    //   chrome.tabs.insertCSS(null, {
-    //     file: 'content.styles.css'
-    //   });
-    //  chrome.tabs.executeScript({
-    //   file: 'contentScript.bundle.js'
-    // });
+    if (!isAppOpen) {
+      window.sessionStorage.setItem('app_open_status', true);
+      console.log('app_open_status is set to true');
+      mountApp();
+    } else {
+      window.sessionStorage.setItem('app_open_status', false);
+      console.log('app_open_status is set to false');
+      unmountApp();
+    }
   }
 }
 
