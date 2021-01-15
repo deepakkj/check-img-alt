ab3f5a17ee099d9d6a07f8b49e3ad6b437ed4ba8
Deepak Kumar Jain <deepakkumarjain21@gmail.com>
2021-01-16 00:58:47 +0530

    convert tsx file to jsx

--- src/pages/Options/Options.jsx
+++ src/pages/Options/Options.jsx
@@ -1,11 +1,7 @@
 import React from 'react';
 import './Options.css';
 
-interface Props {
-  title: string;
-}
-
-const Options: React.FC<Props> = ({ title }: Props) => {
+const Options = ({ title }) => {
   return <div className="OptionsContainer">{title.toUpperCase()} PAGE</div>;
 };
 
